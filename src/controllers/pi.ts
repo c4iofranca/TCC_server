df483474f858ce21e47dc2e9ee649081653fe837
Caio França <caiobdafranca@gmail.com>
2022-10-02 16:16:10 -0300

    ✨feat: Teste de insert no supabase

--- src/controllers/pi.ts
+++ src/controllers/pi.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
+import { db } from "../supabase";
 
 export abstract class PiController {
   constructor() {}
 
   static async GetHelloWorld(req: Request, res: Response) {
     try {
-      res.json({message: "Hello World"});
+      await db.from("Teste").insert({
+        name: "teste",
+      });
+
+      res.json(true);
     } catch (error) {
       return res.status(500).json({ message: error });
     }
