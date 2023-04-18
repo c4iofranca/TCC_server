a004f31cc464a1ee56ee82214c0fa2eaf0059800
Caio França <caiobdafranca@gmail.com>
2023-04-17 21:52:30 -0300

    ✨feat: Rota valores de predição

--- src/controllers/predict.ts
+++ src/controllers/predict.ts
@@ -13,18 +13,39 @@ export abstract class PredictController {
     const body: IGetPredictOutputResults = req.body;
 
     try {
-      const { data } = await db
+      const y1 = db
         .from(tables.predictOutputs)
         .select("*")
         .order("timestamp", { ascending: false })
         .lt("timestamp", body.endDate)
-        .gt("timestamp", body.startDate);
+        .gt("timestamp", body.startDate)
+        .eq("model_output_name", "y1");
 
-      if (data) {
-        return res.json(data.map(d => {
-            return [new Date(d.timestamp).getTime(), d.value]
-        }))
+      const y2 = db
+        .from(tables.predictOutputs)
+        .select("*")
+        .order("timestamp", { ascending: false })
+        .lt("timestamp", body.endDate)
+        .gt("timestamp", body.startDate)
+        .eq("model_output_name", "y2");
+
+      const [ { data: y1Data}, { data: y2Data} ] = await Promise.all([y1, y2])
+
+      const response: Record<string, number[][]> = {};
+
+      if (y1Data) {
+        response["y1"] = y1Data.map(d => {
+          return [new Date(d.timestamp).getTime(), d.value]
+        })
       }
+
+      if (y2Data) {
+        response["y2"] = y2Data.map(d => {
+          return [new Date(d.timestamp).getTime(), d.value]
+        })
+      }
+      
+      res.json(response)
     } catch (error) {
       return res.status(500).json({ message: error });
     }
